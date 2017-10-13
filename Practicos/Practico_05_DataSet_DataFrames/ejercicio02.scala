1ede70a280bee9a76da31c4254b18d09d5635fb3
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-10-13 00:16:09 -0300

    Cambios

--- Practicos/Practico_05_DataSet_DataFrames/ejercicio02.scala
+++
@@ -1,9 +0,0 @@
-/*
-Ejercicio 2
-===========
-Utilizando los datos anteriores producir una salida que agregue a las
-propiedades los tipos del sujeto como un arreglo, de forma tal que cada linea
-tenga la información de la 4-upla (sujeto, [tipos del sujeto], relación, predicado).
-*/
-
-
